409c851e4d1d8c4018825cd78efeb278f1307f9e
rccacho <rccacho@gmail.com>
2017-02-24 16:17:00 -0800

    add app.js routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,26 @@
 
 
 angular
-  .module('bookApp', []);
+  .module('tunely', ['ngRoute'])
+  .config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function config ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/templates/books-index.html',
+      controllerAs: 'booksIndexCtrl',
+      controller: 'BooksIndexController'
+    })
+    .when('/albums/:id', {
+      templateUrl: '/templates/books-show.html',
+      controllerAs: 'booksShowCtrl',
+      controller: 'BooksShowController'
+    })
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+  });
+}
