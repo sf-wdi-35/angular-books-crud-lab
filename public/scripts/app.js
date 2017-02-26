a1527c8ff9357f4817b3787c1fd38c719d0f905a
westondombroski <westondombroski@gmail.com>
2017-02-26 09:42:37 -0800

    books on the page

---
+++ public/scripts/app.js
@@ -0,0 +1,23 @@
+angular
+  .module('booksApp', ['ngRoute'])
+  .config(config);
+
+config.$inject = ['$routeProvider',  '$locationProvider'];
+function config(   $routeProvider,    $locationProvider   ) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/templates/books-index.html',
+      controllerAs: 'booksIndexCtrl',
+      controller: 'BooksIndexController'
+    })
+    .when('/books/:id', {
+      templateUrl: '/templates/books-show.html',
+      controllerAs: 'booksShowCtrl',
+      controller: 'BooksShowController'
+    });
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+}
