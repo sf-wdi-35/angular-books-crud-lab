b1dd4f960acb2577640b740c5bdf8df403eac14a
cnwilliams <hello.christenwilliams@gmail.com>
2017-02-25 21:16:31 -0800

    adds style/structure to index.html; delete, edit & save buttons, links to books by id

--- public/scripts/controllers/BooksIndexController.js
+++ public/scripts/controllers/BooksIndexController.js
@@ -7,30 +7,57 @@ angular
 BooksIndexController.$inject = ["$http"];
 function BooksIndexController(   $http ) {
   var vm = this;
+  vm.newBook = {};
 
   $http({
     method: "GET",
     url: "https://super-crud.herokuapp.com/books"
   }).then(function successCallback(response) {
-    console.log("Here is the response.data.books: ", response.data.books);
     vm.books = response.data.books;
     console.log("here is the vm.books: ", vm.books);
   }, function errorCallback(response) {
     console.log("Error getting the data: ", response);
   }); // end of GET all request
 
-  vm.createBook = function() {
+
+   $http({
+    method: "GET",
+    url: "https://super-crud.herokuapp.com/books"
+  }).then(function successCallback(response) {
+    vm.books = response.data.books;
+    console.log("here is vm.books: ", vm.books);
+  }, function errorCallback(response) {
+    console.log("Error getting the data: ", response);
+  }); // end of GET all request
+
+
+
+
+  vm.editBook = function(book) {
     $http({
-      method: "POST",
-      url: "https://super-crud.herokuapp.com/books",
-      data: vm.newBook,
+      method: "PUT",
+      url: "https://super-crud.herokuapp.com/books" + book._id,
+      data: book
     }).then(function successCallback(response) {
-      console.log("response.data ", response.data)
-      // vm.books.push(response.data);
-    }, function errorCallback(response) {
-      console.log("There was an error posting the new book!");
+      console.log("Edit was successful!");
+      console.log("response.data ", response.data);
+      }, function errorCallback(response) {
+      console.log("There was an error editing the  book!");
     });
-  } // end of POST request
+  } // end of PUT request
+
+
+  vm.deleteBook = function(book) {
+    $http({
+      method: "DELETE",
+      url: "https://super-crud.herokuapp.com/books" + book._id
+    }).then(function successCallback(json){
+      var index = vm.book.indexOf(book);
+      vm.book.splice(index, 1);
+    }, function errorCallback(response) {
+      console.log("there was an error deleting the book :(");
+    });
+  } // end of DELETE request
 
 }; // end of BooksIndexController
 
--- public/scripts/controllers/BooksShowController.js
+++ public/scripts/controllers/BooksShowController.js
@@ -4,17 +4,10 @@ angular
   .module("angularBooks")
   .controller("BooksShowController", BooksShowController);
 
-
 BooksShowController.$inject = ["$http", "$routeParams"];
 
-console.log("ABOUT to hit BooksShowController function ");
-
-
 function BooksShowController(   $http,   $routeParams ) {
-  console.log("BooksShowController function evaluating");
-
   var vm = this;
-  console.log($routeParams);
 
   $http({
     method: "GET",
