ca66af23312301b13b4ff1241dc82f9d0f82c8bb
rccacho <rccacho@gmail.com>
2017-02-25 22:32:14 -0800

    add delete route

--- public/scripts/controllers/BooksShowController.js
+++ public/scripts/controllers/BooksShowController.js
@@ -14,4 +14,17 @@ function BookShowController ($http, $routeParams, $location) {
   }, function errorCallback(response) {
     console.log('There was an error posting the data', response);
   });
+
+  vm.deleteBook = function(book) {
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
+    }).then(function deleteSuccess(response) {
+      $location.path('/')
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
+  
+
 }
