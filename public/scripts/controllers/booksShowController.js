d4a655cea29607d068496a3529785c1e7bfb647c
westondombroski <westondombroski@gmail.com>
2017-02-26 15:06:37 -0800

    buttons

--- public/scripts/controllers/booksShowController.js
+++ public/scripts/controllers/booksShowController.js
@@ -19,9 +19,25 @@ function BooksShowController (   $http,   $routeParams  ) {
       method: 'DELETE',
       url: 'https://super-crud.herokuapp.com/books/' + book._id
     }).then(function onSuccess (res) {
-      vm.book.splice((vm.book.indexOf(book)), 1);
+      //vm.book.splice((vm.book.indexOf(book)), 1);
+      console.log('deleted', res)
     }, function onError(res) {
       console.log(res);
     });
+  }
+
+  vm.editBook = function (book) {
+  $http({
+    method: 'PUT',
+    url: 'https://super-crud.herokuapp.com/books/' + book._id,
+    data: book
+  }).then(function onSuccess(res) {
+  }, function onError(res) {
+    console.log(res);
+  });
+}
+
+  vm.goBack = function () {
+    $location.history.back();
   }
 }
