d37ca71ca725e614db0f603c701e8583f0128699
Will Kaspar <Will@Aang.local>
2017-02-25 18:30:17 -0800

    edit fields work, attaching buttons to actions

--- public/scripts/controllers/booksShowController.js
+++ public/scripts/controllers/booksShowController.js
@@ -11,19 +11,42 @@ function BooksShowController($http, $routeParams, $location){
   var vm = this;
   var bookId = $routeParams.id;
 
+
+  // GRAB & SHOW ONE BOOK //
   $http({
     method: 'GET',
     url: 'https://super-crud.herokuapp.com/books/' + bookId
   }).then(function onSuccess(res){
     vm.book = res.data;
-    console.log('ajax book: ', vm.book);
+    console.log('showjax: ', vm.book);
   }, function onError(res){
     console.log('failjax: ', res.data);
   });
 
+  // UPDATE A BOOK //
+  vm.updateBook = function(bookToUpdate) {
+    console.log('updating: ', bookToUpdate);
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/' + bookToUpdate._id,
+      data: {
+        title: bookToUpdate.title,
+        author: bookToUpdate.author,
+        image: bookToUpdate.image,
+        releaseDate: bookToUpdate.releaseDate
+      }
+    }).then(function onSuccess(res){
+      console.log('updatejax: ', res);
+      vm.book = res.data;
+      $location.path('/');
+    }, function onError(res){
+      console.log('failjax update: ', res.data);
+    })
+  };
+
+  // DELETE A BOOK //
   vm.deleteBook = function(book){
     console.log('deleting: ', book);
-
     $http({
       method: 'DELETE',
       url: 'https://super-crud.herokuapp.com/books/' + bookId
