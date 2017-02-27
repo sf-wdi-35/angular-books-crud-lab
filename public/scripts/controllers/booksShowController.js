acff8f4792c7780312f25f527f1d1cb2f5dc876e
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-27 00:32:37 -0800

    added comments

--- public/scripts/controllers/booksShowController.js
+++ public/scripts/controllers/booksShowController.js
@@ -7,14 +7,14 @@ function BooksShowController (  $http, 	 $routeParams,	 $location ) {
 	var vm = this;
 	var bookId = $routeParams.id;
 	vm.book;
-	vm.bookCopy;
+	vm.bookCopy; //Will use to edit input fields without changing the values displayed in the view until they are saved
 
 	$http({
 		method: 'GET',
 		url: 'https://super-crud.herokuapp.com/books/' + bookId,
 	}).then(function successCallback(response){
 		vm.book = response.data;
-		vm.bookCopy = angular.copy(vm.book);
+		vm.bookCopy = angular.copy(vm.book); //Makes a copy of vm.books and sets it to booksCopy
 
 	}, function errorCallback(error) {
 		console.log('Error: ', error);
