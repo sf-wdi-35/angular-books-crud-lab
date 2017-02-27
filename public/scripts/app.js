419e9e42ce51d09b7f7ee3b1c9951f89b939a6b4
ivannash23 <ivannash23@Gmail.com>
2017-02-27 09:25:49 -0800

    cutting in close

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,15 +6,18 @@ config.$inject = ['$routeProvider', '$locationProvider'];
 function config (  $routeProvider ,  $locationProvider ) {
   $routeProvider
     .when('/', {
-      templateUrl: '/templates/books-index.html',
+      templateUrl: '/templates/books/books-index.html',
       controllerAs: 'booksIndexCtrl',
       controller: 'BooksIndexController'
     })
     .when('/books/:id', {
-      templateUrl: '/templates/books-show.html',
+      templateUrl: '/templates/books/books-show.html',
       controllerAs: 'booksShowCtrl',
       controller: 'BooksShowController'
     })
+    .otherwise({
+      redirectTo: '/'
+    });
 
     $locationProvider.html5Mode({
       enabled: true,
