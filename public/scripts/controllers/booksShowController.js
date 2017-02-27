aa23f9c29114f63321a41c11140bfc222bde467f
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-26 18:47:24 -0800

    delete button working and rerouting to home

--- public/scripts/controllers/booksShowController.js
+++ public/scripts/controllers/booksShowController.js
@@ -2,8 +2,8 @@ angular
 	.module('bookApp')
 	.controller('BooksShowController', BooksShowController);
 
-BooksShowController.$inject = ['$http', '$routeParams'];
-function BooksShowController (  $http, 	 $routeParams) {
+BooksShowController.$inject = ['$http', '$routeParams', '$location'];
+function BooksShowController (  $http, 	 $routeParams,	 $location ) {
 	var vm = this;
 	var bookId = $routeParams.id;
 
@@ -12,10 +12,22 @@ function BooksShowController (  $http, 	 $routeParams) {
 		url: 'https://super-crud.herokuapp.com/books/' + bookId,
 	}).then(function successCallback(response){
 		vm.book = response.data;
-		console.log(vm.book)
 	}, function errorCallback(error) {
-		console.log('Error', error);
+		console.log('Error: ', error);
 	})
+
+	vm.deleteBook = function(book){
+		$http({
+			method: 'DELETE',
+			url: 'https://super-crud.herokuapp.com/books/' + bookId
+		}).then(function deleteSuccessCallback(response){
+			//redirect to homepage
+			$location.path('/')
+			// console.log(response);
+		}, function deleteErrorCallback(error){
+			console.log('Error: ', error);
+		})
+	}
 
 
 }
