9973550c7dc76f53871ea41e119cd15fb4bfa01f
cnwilliams <hello.christenwilliams@gmail.com>
2017-02-26 18:07:45 -0800

    detele WORKS

--- public/scripts/controllers/BooksIndexController.js
+++ public/scripts/controllers/BooksIndexController.js
@@ -1,3 +1,75 @@
+// console.log("BooksIndexController running!!!!")
+
+// angular
+//   .module("angularBooks")
+//   .controller("BooksIndexController", BooksIndexController);
+
+// BooksIndexController.$inject = ["$http"];
+// function BooksIndexController(   $http ) {
+//   var vm = this;
+//   vm.newBook = {};
+
+//   $http({
+//     method: "GET",
+//     url: "https://super-crud.herokuapp.com/books"
+//   }).then(function successCallback(response) {
+//     vm.books = response.data.books;
+//     console.log("here is the vm.books: ", vm.books);
+//   }, function errorCallback(response) {
+//     console.log("Error getting the data: ", response);
+//   }); // end of GET all request
+
+
+//    $http({
+//     method: "GET",
+//     url: "https://super-crud.herokuapp.com/books"
+//   }).then(function successCallback(response) {
+//     vm.books = response.data.books;
+//     console.log("here is vm.books: ", vm.books);
+//   }, function errorCallback(response) {
+//     console.log("Error getting the data: ", response);
+//   }); // end of GET all request
+
+
+
+
+//   vm.editBook = function(book) {
+//     $http({
+//       method: "PUT",
+//       url: "https://super-crud.herokuapp.com/books" + book._id,
+//       data: {
+//         title: book.title,
+//         author: book.author,
+//         releaseDate: book.releaseDate,
+//         image: book.image
+//       }
+//     }).then(function successCallback(response) {
+//         console.log("Edit was successful!");
+//         console.log("response.data ", response.data);
+//         }, function errorCallback(response) {
+//         console.log("There was an error editing the  book!");
+//     })
+//   } // end of PUT request
+
+
+//   vm.deleteBook = function(book) {
+//     $http({
+//       method: "DELETE",
+//       url: "https://super-crud.herokuapp.com/books" + book._id
+//     }).then(function successCallback(response){
+//       // var index = vm.book.indexOf(book);
+//       // vm.book.splice(index, 1);
+//     }, function errorCallback(response) {
+//       console.log("there was an error deleting the book :(");
+//     });
+//   } // end of DELETE request
+
+// }; // end of BooksIndexController
+
+
+
+
+
 console.log("BooksIndexController running!!!!")
 
 angular
@@ -18,50 +90,6 @@ function BooksIndexController(   $http ) {
   }, function errorCallback(response) {
     console.log("Error getting the data: ", response);
   }); // end of GET all request
-
-
-   $http({
-    method: "GET",
-    url: "https://super-crud.herokuapp.com/books"
-  }).then(function successCallback(response) {
-    vm.books = response.data.books;
-    console.log("here is vm.books: ", vm.books);
-  }, function errorCallback(response) {
-    console.log("Error getting the data: ", response);
-  }); // end of GET all request
-
-
-
-
-  vm.editBook = function(book) {
-    $http({
-      method: "PUT",
-      url: "https://super-crud.herokuapp.com/books" + book._id,
-      data: {
-        title: book.title,
-        author: book.author,
-        releaseDate: book.releaseDate,
-        image: book.image
-      }).then(function successCallback(response) {
-        console.log("Edit was successful!");
-        console.log("response.data ", response.data);
-        }, function errorCallback(response) {
-        console.log("There was an error editing the  book!");
-    });
-  } // end of PUT request
-
-
-  vm.deleteBook = function(book) {
-    $http({
-      method: "DELETE",
-      url: "https://super-crud.herokuapp.com/books" + book._id
-    }).then(function successCallback(response){
-      // var index = vm.book.indexOf(book);
-      // vm.book.splice(index, 1);
-    }, function errorCallback(response) {
-      console.log("there was an error deleting the book :(");
-    });
-  } // end of DELETE request
 
 }; // end of BooksIndexController
 
