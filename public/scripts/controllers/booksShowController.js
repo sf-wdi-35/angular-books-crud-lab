8723071ef5f3c7c7ec60d05be819948f299192a2
westondombroski <westondombroski@gmail.com>
2017-02-26 18:04:25 -0800

    up and running, cancel edit button still giving me issues

--- public/scripts/controllers/booksShowController.js
+++ public/scripts/controllers/booksShowController.js
@@ -1,43 +1,45 @@
 angular
-  .module('booksApp')
-  .controller('BooksShowController', BooksShowController);
+    .module('booksApp')
+    .controller('BooksShowController', BooksShowController);
 
-BooksShowController.$inject = [ '$http', '$routeParams'];
-function BooksShowController (   $http,   $routeParams  ) {
-  var vm = this;
-  $http({
-    method: 'GET',
-    url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
-  }).then(function onSuccess(res) {
-    vm.book = res.data;
-  }, function onError(res){
-    console.log(res);
-  });
+BooksShowController.$inject = ['$http', '$routeParams', '$location'];
 
-  vm.deleteBook = function (book) {
+function BooksShowController($http, $routeParams, $location) {
+    var vm = this;
     $http({
-      method: 'DELETE',
-      url: 'https://super-crud.herokuapp.com/books/' + book._id
-    }).then(function onSuccess (res) {
-      //vm.book.splice((vm.book.indexOf(book)), 1);
-      console.log('deleted', res)
+        method: 'GET',
+        url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
+    }).then(function onSuccess(res) {
+        vm.book = res.data;
     }, function onError(res) {
-      console.log(res);
+        console.log(res);
     });
-  }
 
-  vm.editBook = function (book) {
-  $http({
-    method: 'PUT',
-    url: 'https://super-crud.herokuapp.com/books/' + book._id,
-    data: book
-  }).then(function onSuccess(res) {
-  }, function onError(res) {
-    console.log(res);
-  });
-}
+    vm.deleteBook = function(book) {
+        $http({
+            method: 'DELETE',
+            url: 'https://super-crud.herokuapp.com/books/' + book._id
+        }).then(function onSuccess(res) {
+            $location.path('/');
+        }, function onError(res) {
+            console.log(res);
+        });
+    }
 
-  vm.goBack = function () {
-    $location.history.back();
-  }
+    vm.editBook = function(book) {
+        $http({
+            method: 'PUT',
+            url: 'https://super-crud.herokuapp.com/books/' + book._id,
+            data: book
+        }).then(function onSuccess(res) {
+            $location.path('/');
+        }, function onError(res) {
+            console.log(res);
+        });
+    }
+
+    vm.cancelEdit = function(book) {
+      $location.path('/books/' + book._id);
+      console.log(book);
+    }
 }
